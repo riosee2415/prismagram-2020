ef120e70d5e2ad43b3e7096572ff67e09ebd7796
4leaf.ysh <4leaf.ysh@gmail.com>
2020-07-31 22:40:49 +0900

    create conputed file

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -6,31 +6,62 @@ export default {
       return `${parent.firstName} ${parent.lastName}`;
     },
 
-    amIFollowing: async (parent, _, { request }) => {
+    isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
 
       try {
         const exists = await prisma.$exists.user({
-          AND: [{ id: parentId }, { followers_some: [user.id] }],
+          AND: [
+            { id: parentId },
+            {
+              followers_some: {
+                id: user.id,
+              },
+            },
+          ],
         });
 
-        if (exists) {
-          return true;
-        } else {
-          return false;
-        }
+        return exists;
       } catch (e) {
         console.log(e);
         return false;
       }
     },
 
-    itsMe: (parent, _, { request }) => {
+    isSelf: (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
 
       return user.id === parentId;
+    },
+  },
+
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      // 현재 로그인 된 사용자 정보를 받아온다 [A]
+      const { user } = request;
+
+      // 현재 Post의 id를 받아온다. [B]
+      const { id } = parent;
+
+      //B의 likes 중 A가 있는지 검증하여 리턴한다.
+      const exist = await prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id,
+            },
+          },
+          {
+            post: {
+              id,
+            },
+          },
+        ],
+      });
+
+      return exist;
     },
   },
 };
